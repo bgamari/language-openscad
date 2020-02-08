7db9c2f30f52534c4ed0c366d65c1704133e1ec4
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-02-08 21:37:38 +0000

    Start implementing writeObject

--- src/Language/OpenSCAD/Writer.hs
+++ src/Language/OpenSCAD/Writer.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Language.OpenSCAD.Writer where
 import Language.OpenSCAD
 import qualified Data.ByteString.Char8 as BS
+import Data.Maybe (fromMaybe)
 
 write :: [TopLevel] -> [BS.ByteString]
 write = map writeTopLevel
@@ -12,4 +14,15 @@ writeTopLevel x = case x of
     IncludeDirective str -> BS.pack ("include <" ++ str ++ ">")
 
 writeObject :: Object -> BS.ByteString
-writeObject obj = BS.pack ""
+writeObject obj = case obj of
+     Module (Ident ident) args maybeObj -> BS.pack (ident ++ "() ") `BS.append` fromMaybe BS.empty (fmap writeObject maybeObj)
+     ForLoop ident expr obj     -> undefined
+     Objects objs               -> undefined
+     If expr obj maybeObj       -> undefined
+     BackgroundMod obj          -> undefined
+     DebugMod obj               -> undefined
+     RootMod obj                -> undefined
+     DisableMod obj             -> undefined
+     ModuleDef name args body   -> undefined
+     VarDef name value          -> undefined
+     FuncDef name args body     -> undefined
