5175fe191e993a706b5f2c98f026ec36bd2efd78
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-03-29 19:18:05 +0100

    Use hfmt

--- WriterTest.hs
+++ WriterTest.hs
@@ -1,21 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Main where
-import System.IO
-import Data.Text
-import qualified Data.ByteString.Char8 as BS
-import qualified Data.Text.Prettyprint.Doc as P
-import Data.Text.Prettyprint.Doc.Render.Text (renderIO)
 
-import Language.OpenSCAD
-import Language.OpenSCAD.Writer
+module Main where
+
+import qualified Data.ByteString.Char8                 as BS
+import           Data.Text
+import qualified Data.Text.Prettyprint.Doc             as P
+import           Data.Text.Prettyprint.Doc.Render.Text (renderIO)
+import           System.IO
+
+import           Language.OpenSCAD
+import           Language.OpenSCAD.Writer
 
 main :: IO ()
 main = do
-    scad <- BS.readFile "test.scad"
-    let layoutOptions = P.LayoutOptions {P.layoutPageWidth = P.AvailablePerLine 80 1}
-    case parse scad of
-        Left err -> putStrLn err
-        Right tl ->
-            renderIO System.IO.stdout
-                $ P.layoutPretty layoutOptions $ P.unAnnotate $ pretty tl
-    return ()
+  scad <- BS.readFile "test.scad"
+  let layoutOptions =
+        P.LayoutOptions {P.layoutPageWidth = P.AvailablePerLine 80 1}
+  case parse scad of
+    Left err -> putStrLn err
+    Right tl ->
+      renderIO System.IO.stdout $
+      P.layoutPretty layoutOptions $ P.unAnnotate $ pretty tl
+  return ()
--- src/Language/OpenSCAD/Writer.hs
+++ src/Language/OpenSCAD/Writer.hs
@@ -1,40 +1,44 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Language.OpenSCAD.Writer where
-import Language.OpenSCAD
-import Data.Maybe (fromMaybe, maybeToList)
-import Data.Text.Lazy (Text, pack)
-import Data.Text.Prettyprint.Doc (vsep, group, Doc, line, softline)
+
+import           Data.Maybe                (fromMaybe, maybeToList)
+import           Data.Text.Lazy            (Text, pack)
+import           Data.Text.Prettyprint.Doc (Doc, group, line, softline, vsep)
 import qualified Data.Text.Prettyprint.Doc as P
+import           Language.OpenSCAD
 
 pretty :: [TopLevel] -> Doc Text
 pretty = vsep . map prettyTopLevel
 
-
 prettyTopLevel :: TopLevel -> Doc Text
-prettyTopLevel x = case x of
+prettyTopLevel x =
+  case x of
     TopLevelScope obj    -> prettyObject obj
     UseDirective str     -> "use" </> "<" <> t str <> ">"
     IncludeDirective str -> "include" </> "<" <> t str <> ">"
 
-
 prettyObject :: Object -> Doc Text
-prettyObject obj = case obj of
-     Module (Ident ident) args maybeObj -> group $ t ident
-            <> "()"
-            <> fromMaybe ";" (fmap (mappend line . prettyObject) maybeObj)
-     ForLoop ident expr obj     -> undefined
-     Objects objs               -> undefined
-     If expr obj maybeObj       -> undefined
-     BackgroundMod obj          -> undefined
-     DebugMod obj               -> undefined
-     RootMod obj                -> undefined
-     DisableMod obj             -> undefined
-     ModuleDef name args body   -> undefined
-     VarDef name value          -> undefined
-     FuncDef name args body     -> undefined
+prettyObject obj =
+  case obj of
+    Module (Ident ident) args maybeObj ->
+      group $
+      t ident <>
+      "()" <> fromMaybe ";" (fmap (mappend line . prettyObject) maybeObj)
+    ForLoop ident expr obj   -> undefined
+    Objects objs             -> undefined
+    If expr obj maybeObj     -> undefined
+    BackgroundMod obj        -> undefined
+    DebugMod obj             -> undefined
+    RootMod obj              -> undefined
+    DisableMod obj           -> undefined
+    ModuleDef name args body -> undefined
+    VarDef name value        -> undefined
+    FuncDef name args body   -> undefined
 
 t :: String -> Doc Text
 t = P.pretty . pack
 
 x <$> y = x <> line <> y
+
 x </> y = x <> softline <> y
