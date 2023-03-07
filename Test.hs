561bf6128bc4f350e6c605c449c931b95e94ffd4
Brecht Serckx <professional@brechtserckx.be>
2023-03-07 19:02:57 +0100

    Rename tests

--- Test.hs
+++ Test.hs
@@ -20,7 +20,7 @@ import Test.Tasty.Silver
 main :: IO ()
 main = do
     testTree <- getTests
-    defaultMain $ testGroup "tests" [testTree, roundTripTests, prettyTests, parseTests]
+    defaultMain $ testGroup "tests" [testTree, roundtripTests, exactprintTests, parseTests]
 
 getTests :: IO TestTree
 getTests = do
@@ -31,8 +31,8 @@ getTests = do
         , let base = takeBaseName file
         ]
 
-roundTripTests :: TestTree
-roundTripTests = testGroup "roundtrip tests"
+roundtripTests :: TestTree
+roundtripTests = testGroup "roundtrip tests"
   [ roundtrip "ident" ident
   , roundtrip "expression" expression
   , roundtrip "object" object
@@ -56,32 +56,32 @@ dumpScad file = do
       Left err -> fail err
       Right a  -> return $ ppShow a
 
-prettyTests :: TestTree
-prettyTests = testGroup "pretty tests"
+exactprintTests :: TestTree
+exactprintTests = testGroup "exactprint tests"
   [ testGroup "Module"
-    [ testFormat 80 [s|
+    [ testExactprint 80 [s|
 myModule();|]
-    , testFormat 10 [s|
+    , testExactprint 10 [s|
 myModule();|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myModule(arg1);|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myModule(arg1, arg2);|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myModule(arg1 = 1.0, arg2);|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myModule() myModule2();|]
-    , testFormat 20 [s|
+    , testExactprint 20 [s|
 myModule( arg1
         , arg2 );|]
-    , testFormat 20 [s|
+    , testExactprint 20 [s|
 myModule()
   myModule2();|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myModule() { foo(); }|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myModule() { foo(); bar(); }|]
-    , testFormat 15 [s|
+    , testExactprint 15 [s|
 myModule() {
   foo();
   bar();
@@ -89,184 +89,184 @@ myModule() {
 }|]
     ]
   , testGroup "ForLoop"
-    [ testFormat 80 [s|
+    [ testExactprint 80 [s|
 for(myVar = 1.0) myModule();|]
-    , testFormat 20 [s|
+    , testExactprint 20 [s|
 for(myVar = 1.0)
   myModule();|]
     ]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 for(myVar = true) { foo(); bar(); }|]
-    , testFormat 15 [s|
+    , testExactprint 15 [s|
 for(myVar = true) {
   foo();
   bar();
   baz();
 }|]
   , testGroup "Objects"
-    [ testFormat 80 [s|
+    [ testExactprint 80 [s|
 { myModule1(); myModule2(); }|]
-    , testFormat 20 [s|
+    , testExactprint 20 [s|
 {
   myModule1();
   myModule2();
 }|]
     ]
   , testGroup "If"
-    [ testFormat 80 [s|
+    [ testExactprint 80 [s|
 if (true) myModule();|]
-    , testFormat 20 [s|
+    , testExactprint 20 [s|
 if (true)
   myModule();|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 if (true) { myModule(); myModule(); }|]
-    , testFormat 20 [s|
+    , testExactprint 20 [s|
 if (true) {
   myModule();
   myModule();
 }|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 if (true) if (true) foo();|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 if (true) { if (true) foo(); else bar(); }|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 if (true) { if (true) foo(); } else bar();|]
     ]
   , testGroup "*Mod"
-    [ testFormat 80 [s|
+    [ testExactprint 80 [s|
 %myModule();|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 #myModule();|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 !myModule();|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 *myModule();|]
     ]
   , testGroup "ModuleDef"
-    [ testFormat 80 [s|
+    [ testExactprint 80 [s|
 module myModule() {}|]
-    , testFormat 10 [s|
+    , testExactprint 10 [s|
 module myModule() {}|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 module myModule(arg1, arg2, arg3 = true) {}|]
-    , testFormat 10 [s|
+    , testExactprint 10 [s|
 module myModule( arg1
                , arg2
                , arg3 = true ) {}|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 module myModule() { myModule2(); myModule2(); }|]
-    , testFormat 20 [s|
+    , testExactprint 20 [s|
 module myModule() {
   myModule2();
   myModule2();
 }|]
     ]
   , testGroup "VarDef"
-    [ testFormat 80 [s|
+    [ testExactprint 80 [s|
 myVar = true;|]
-    , testFormat 10 [s|
+    , testExactprint 10 [s|
 myVar
   = true;|]
     ]
   , testGroup "FuncDef"
-    [ testFormat 80 [s|
+    [ testExactprint 80 [s|
 function myFunc() = true;|]
-    , testFormat 10 [s|
+    , testExactprint 10 [s|
 function myFunc()
   = true;|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 function myFunc(arg1, arg2) = true;|]
-    , testFormat 10 [s|
+    , testExactprint 10 [s|
 function myFunc( arg1
                , arg2 )
   = true;|]
     ]
   , testGroup "Expr"
-    [ testFormat 80 [s|
+    [ testExactprint 80 [s|
 myVar = myVar2;|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = myVar2[foo];|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = 1.0;|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = [true, false];|]
-    , testFormat 20 [s|
+    , testExactprint 20 [s|
 myVariables
   = [aaa, b];|]
-    , testFormat 10 [s|
+    , testExactprint 10 [s|
 myVar
   = [ true
     , false ];|]
     ]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = [true:false];|]
-    , testFormat 25 [s|
+    , testExactprint 25 [s|
 myVariables
   = [true:false];|]
-    , testFormat 15 [s|
+    , testExactprint 15 [s|
 myVar
   = [ true
     : false ];|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = "myString";|]
     , testGroup "string escapings"
-      [ testFormat 80 (mconcat ["myVar = \"" <> s <> "\";"])
+      [ testExactprint 80 (mconcat ["myVar = \"" <> s <> "\";"])
       | s <- ["\\\"", "\\\\", "\\t", "\\n", "\\r"]
       ]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = true;|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = false;|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = myFunc();|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = myFunc(arg1, arg2);|]
-    , testFormat 25 [s|
+    , testExactprint 25 [s|
 myVar
   = myFunc(arg1, arg2);|]
-    , testFormat 20 [s|
+    , testExactprint 20 [s|
 myVar
   = myFunc( arg1
           , arg2 );|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = !myVar2;|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = -myVar2;|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = -(myVar2);|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = a + b;|]
-    , testFormat 15 [s|
+    , testExactprint 15 [s|
 myVariable
   = aaaaa + b;|]
-    , testFormat 10 [s|
+    , testExactprint 10 [s|
 myVarable
   = aaaaa
     + b;|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = a ? b : c;|]
-    , testFormat 15 [s|
+    , testExactprint 15 [s|
 myVar
   = a ? b : c;|]
-    , testFormat 10 [s|
+    , testExactprint 10 [s|
 myVar
   = aaaa
     ? b
     : c;|]
-    , testFormat 80 [s|
+    , testExactprint 80 [s|
 myVar = (a + b);|]
-    , testFormat 15 [s|
+    , testExactprint 15 [s|
 myVariable
   = (a + b);|]
-    , testFormat 10 [s|
+    , testExactprint 10 [s|
 myVar
   = ( aaaaa
       + b
       + c );|]
   ]
  where
-   testFormat :: HasCallStack => Int -> String -> TestTree
-   testFormat w src = testCase src $ case parse (BS8.pack src) of 
+   testExactprint :: HasCallStack => Int -> String -> TestTree
+   testExactprint w src = testCase src $ case parse (BS8.pack src) of 
      Left e -> assertFailure $ "Parse failure: " <> e
      Right [tl] ->
        let opts = PP.LayoutOptions $ PP.AvailablePerLine w 1
