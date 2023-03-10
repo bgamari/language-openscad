7b06df5a4dcd5679e0438081b0e72d70bc66433e
Brecht Serckx <services+git@brechtserckx.be>
2023-03-10 18:11:06 +0100

    Ormolu formatting

--- dump_scad.hs
+++ dump_scad.hs
@@ -1,9 +1,9 @@
 module Main where
 
 import qualified Data.ByteString.Char8 as BS
-import           Language.OpenSCAD
-import           System.Environment
-import           Text.Show.Pretty      (ppShow)
+import Language.OpenSCAD
+import System.Environment
+import Text.Show.Pretty (ppShow)
 
 main :: IO ()
 main = do
@@ -16,4 +16,4 @@ dumpScad file = do
   result <- parse <$> BS.readFile file
   case result of
     Left err -> fail err
-    Right a  -> return $ ppShow a
+    Right a -> return $ ppShow a
